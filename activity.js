6a2c78dc1d18183b2bccedf9a3bc5ad149b24cfb
revanth05-tech <revanthyegireddi@gmail.com>
2026-07-16 11:22:23 +0530

    build complete github Activity-cli

--- activity.js
+++ activity.js
@@ -1,26 +1,38 @@
-const username = process.argv[2]
-const fs = require('fs');
-const path = require('path');
-
+const username = process.argv[2];
 
 if(!username){
     console.log("Please Provide a Username🫡");
     process.exit(1);
 }
 
-async function getActivity(){
+async function getActivity() {
     const url = `https://api.github.com/users/${username}/events`;
-    const response = await fetch(url);
-
-    const data = await response.json();
-    for(const event of data){
-        if(event.type == "PushEvent"){
-            console.log(`Pushed to ${event.repo.name}`)
-        }else if(event.type == "CreateEvent"){
-            console.log(`Created ${event.payload.ref_type} in ${event.repo.name}`);
-        }else if(event.type == "WatchEvent"){
-            console.log(`Starred at ${event.repo.name}`);
+    try{
+        const response = await fetch(url);
+        if (!response.ok) {
+            console.log(`User "${username}" not found...😵`);
+            return;
         }
+        const data = await response.json();
+        for (const event of data) {
+            switch(event.type){
+                case "PushEvent":
+                    console.log(`Pushed to ${event.repo.name}`);
+                    break;
+                case "CreateEvent":
+                    console.log(`Created ${event.payload.ref_type} in ${event.repo.name}`);
+                    break;
+                case "WatchEvent":
+                    console.log(`Starred ${event.repo.name}`);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }catch(error){
+        console.log(`Unable to connect the Github and GithubUser:${username}...😵`);
+        console.error(error.message);
     }
 }
+
 getActivity();
