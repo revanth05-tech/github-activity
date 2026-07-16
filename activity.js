e23016c8f7323debc7eb14925d726f6b11119ace
revanth05-tech <revanthyegireddi@gmail.com>
2026-07-16 10:56:39 +0530

    github events activity has been implement

--- activity.js
+++ activity.js
@@ -8,4 +8,19 @@ if(!username){
     process.exit(1);
 }
 
-console.log(`GitHub Username: ${username}`)
+async function getActivity(){
+    const url = `https://api.github.com/users/${username}/events`;
+    const response = await fetch(url);
+
+    const data = await response.json();
+    for(const event of data){
+        if(event.type == "PushEvent"){
+            console.log(`Pushed to ${event.repo.name}`)
+        }else if(event.type == "CreateEvent"){
+            console.log(`Created ${event.payload.ref_type} in ${event.repo.name}`);
+        }else if(event.type == "WatchEvent"){
+            console.log(`Starred at ${event.repo.name}`);
+        }
+    }
+}
+getActivity();
